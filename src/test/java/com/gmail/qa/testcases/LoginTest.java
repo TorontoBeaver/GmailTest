12891f75cc38f9ea4679a21f887253fd135fabbe
Adam Ross <adamross2018>
2018-09-04 23:58:10 -0400

    and properties

--- src/test/java/com/gmail/qa/testcases/LoginTest.java
+++ src/test/java/com/gmail/qa/testcases/LoginTest.java
@@ -6,7 +6,32 @@ import com.gmail.qa.pages.LogInPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class LoginTest {
+
+	public static Properties prop;
+
+	public LoginTest() {
+
+		prop = new Properties();
+		FileInputStream ip = null;
+		try {
+			ip = new FileInputStream("/Users/vladimirnemtcev/Downloads/GmailTest/src/main/java/com/gmail/qa/config/config.properties");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			prop.load(ip);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 
 	@Test
 
@@ -18,7 +43,9 @@ public class LoginTest {
 		System.out.println("Starting positive Login test");
 
 		LogInPage logInPage = introductionPage.openSignInPage();
-		HomePage homePage = logInPage.userLogIn("potlightexpress", "Vlad1961$");
+
+		HomePage homePage = logInPage.userLogIn(prop.getProperty("username"), prop.getProperty("password"));
+
 		Assert.assertTrue(homePage.isUserAvatarDisplayed());
 
 	}
@@ -31,11 +58,11 @@ public class LoginTest {
 
 		introductionPage.open();
 
-		System.out.println("Starting incorrect userName test");
+		System.out.println("Starting incorrect LogIn useID test");
 
 		LogInPage logInPage = introductionPage.openSignInPage();
 
-		HomePage homePage = logInPage.wrongUserLoginID("wrongUserLoginID");
+		HomePage homePage = logInPage.wrongUserLoginID(prop.getProperty("wrongUserID"));
 
 		Assert.assertTrue(logInPage.isUWrongUserNameErrorMessageDisplayed());
 
@@ -49,11 +76,11 @@ public class LoginTest {
 
 		introductionPage.open();
 
-		System.out.println("Starting incorrect userPassword test");
+		System.out.println("Starting incorrect LogIn userPassword test");
 
 		LogInPage logInPage = introductionPage.openSignInPage();
 
-		HomePage homePage = logInPage.wrongUserLoginPassword("potlightexpress","wrongPassword");
+		HomePage homePage = logInPage.wrongUserLoginPassword(prop.getProperty("username"), prop.getProperty("wrongPassword"));
 
 		Assert.assertTrue(logInPage.isWrongPasswordErrorMessageDisplayed());
 
